44133d42b10c49da872ba939aa2c6ff41b8253ac
Cole Hao Wu <hwa129@sfu.ca>
2021-05-01 15:04:13 -0700

    basic implement of cart

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -7,7 +7,11 @@ import { ProductsService } from './products.service'
 })
 
 export class CartService {
-  products: CartItem[] = [];
+  // products: CartItem[] = [];
+  products: CartItem[] = [
+    new CartItem(new Product('drake Lil Yachty', 100, '', 'drakeLilYachtyTemplate.png', 'Template'), 5),
+    new CartItem(new Product('Average Fan Vs Average Enjoyer', 100, '', 'AverageFanVsAverageEnjoyer.jpg', 'Template'), 2),
+  ];
   constructor(private ps: ProductsService) { }
   add(id: string, quantity: number){
     this.products.push(new CartItem(this.ps.getProductById(id), quantity));
@@ -19,7 +23,7 @@ export class CartService {
   }
 }
 
-class CartItem {
+export class CartItem {
   product: Product;
   quantity: number;
   constructor(product: Product, quantity) {
