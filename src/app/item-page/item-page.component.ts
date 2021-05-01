0afc001b5151968322d2822a5c9b87dd4a058cfe
Cole Hao Wu <hwa129@sfu.ca>
2021-05-01 12:54:01 -0700

    add service

--- src/app/item-page/item-page.component.ts
+++ src/app/item-page/item-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../products.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-item-page',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemPageComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private ar: ActivatedRoute, private ps: ProductsService) { }
+  productID;
+  a = 'aloha';
   ngOnInit(): void {
+    this.productID = this.ar.snapshot.paramMap.get('id');
+    this.ps.getProductById(this.productID);
   }
 
 }
