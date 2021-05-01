f79ee6a78b4cced83841ca1a650da0368fec1d0e
Cole Hao Wu <hwa129@sfu.ca>
2021-05-01 11:00:23 -0700

    components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ItemPageComponent } from './item-page/item-page.component';
+import { CartPageComponent } from './cart-page/cart-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ItemPageComponent,
+    CartPageComponent
   ],
   imports: [
     BrowserModule,
