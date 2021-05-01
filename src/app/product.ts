f95d36f50c287b48e609a46edaff19ad3cf31e78
Cole Hao Wu <hwa129@sfu.ca>
2021-05-01 12:10:46 -0700

    adding ID

--- src/app/product.ts
+++ src/app/product.ts
@@ -4,7 +4,9 @@ export class Product {
     description: string;
     picSrc: string; // relative path for item's picture
     category: string;
+    static id: number = 0;
     constructor(name?: string, price?: number, description?: string, picSrc?: string, category?: string){
+        Product.id ++;
         this.description = description;
         this.name = name;
         this.picSrc = picSrc;
