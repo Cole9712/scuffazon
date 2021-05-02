b94185cbaf7c9b3373966dda54bc5815c6b6d79f
Ross Shen <rossshen@qq.com>
2021-05-01 18:14:43 -0700

    added one more meme to product service

--- src/app/products.service.ts
+++ src/app/products.service.ts
@@ -12,6 +12,7 @@ export class ProductsService {
 	new Product('Hackathon Programmers Vs Speedrunners', 200, '', 'HackathonProgrammersVsSpeedrunners.png', 'Meme', true, 3152, 5),
 	new Product('I Recieve You Receive', 100, '', 'iRecieveYouReceive.png', 'Template', false, 7120, 4),
 	new Product('Nobody Is Born Cool Scuffed Hacks', 200, '', 'nobodyIsBornCoolScuffedHacks.jpg.png', 'Meme', true, 1242, 5),
+	new Product('Programmming Is Hard', 1000, '', 'ProgrammmingIsHard.png', 'Meme', true, 9239, 5)
   ];
   
   constructor() { }
