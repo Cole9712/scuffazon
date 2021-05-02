7b1d1deb9cd7f9bc45cdf65c2f9b5923df02fc22
Cole Hao Wu <hwa129@sfu.ca>
2021-05-01 17:17:32 -0700

    add to cart function

--- src/app/item-page/item-page.component.ts
+++ src/app/item-page/item-page.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ProductsService } from '../products.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CartService } from '../cart.service'
 
 @Component({
   selector: 'app-item-page',
@@ -9,14 +10,21 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ItemPageComponent implements OnInit {
 
-  constructor(private ar: ActivatedRoute, private ps: ProductsService) { }
+  constructor(private ar: ActivatedRoute, private ps: ProductsService, private cs:CartService, private router : Router) { }
   
   productID;
   product;
+  @ViewChild('qty') qty;
 
   ngOnInit(): void {
     this.productID = this.ar.snapshot.paramMap.get('id');
     this.product = this.ps.getProductById(this.productID);
+  }
+
+  addToCart(){
+    console.log(this.qty);
+    this.cs.add(this.productID, parseInt(this.qty.nativeElement.value));
+    this.router.navigateByUrl["/cart"];
   }
 
 }
